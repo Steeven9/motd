c0c903dcdee797f13f796d1a34f7e14822ed4da8
Stefano Taillefert <stefano.taille@gmail.com>
2025-10-12 21:26:39 +0200

    Update scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -22,10 +22,15 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 	curl -L https://github.com/Steeven9/motd/archive/master.tar.gz | tar -zxv
 
 	# Move old motd files to directory
-	old_folder_name="/etc/update-motd.d/old-motd-$(date +%s)"
+	old_folder_name="/etc/update-motd-bkup-$(date +%s)"
 	echo "Backing up old files to ${old_folder_name}"
 	mkdir $old_folder_name
 	mv /etc/update-motd.d/* $old_folder_name
+
+	# Put back default updates and reboot notifications
+	cp "${old_folder_name}/90-updates-available" /etc/update-motd.d
+	cp "${old_folder_name}/91-release-upgrade" /etc/update-motd.d
+	cp "${old_folder_name}/98-reboot-required" /etc/update-motd.d
 
 	# Move unzipped motd files to /etc
 	echo "Installing motd"
@@ -37,6 +42,11 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 	fi
 	echo "Setting permissions"
 	chmod +x /etc/update-motd.d/*
+
+	# Shush default "last login" line for SSH
+	cp /etc/ssh/sshd_config /etc/ssh/sshd_config.backup
+	sed -i.bak "s/#PrintLastLog yes/PrintLastLog no/" /etc/ssh/sshd_config
+	service ssh restart
 
 	# Clean up downloaded files
 	echo "Cleaning up"
